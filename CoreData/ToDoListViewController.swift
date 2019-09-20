736c3d710d254952e30eea864cf8603ecfe2e416
Shetora <shetora@MBP-Anton.local>
2019-09-20 18:53:40 +0300

    Make

--- CoreData/ToDoListViewController.swift
+++ CoreData/ToDoListViewController.swift
@@ -17,15 +17,31 @@ class ToDoListViewController: UITableViewController {
         cell.textLabel?.text = data[indexPath.row]
         return cell
     }
+    
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print (data[indexPath.row])
-        
         if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
             tableView.cellForRow(at: indexPath)?.accessoryType = .none
         } else {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    @IBAction func addNewItemsButton(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Create new", message: "", preferredStyle: .alert)
+        
+        alert.addTextField { (alertText) in
+            alertText.placeholder = "Enter the name"
+            textField = alertText
+        }
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            self.data.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addAction(action)
+        present(alert, animated: true)
+        
     }
 }
 
